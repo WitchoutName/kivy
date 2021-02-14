c3196ad5e8a8a421e8478bd113c16a70a791a147
Witchoutname <folyn.tomas1@gmail.com>
2021-02-14 21:36:49 +0100

    init

--- main.py
+++ main.py
@@ -3,23 +3,42 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
 
 
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
-        self.cols = 3
-        self.add_widget(Label(text="Name: "))
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.inside.add_widget(Label(text="Name: "))
         self.name = TextInput(multiline=False)
-        self.add_widget(self.name)
+        self.inside.add_widget(self.name)
 
-        self.add_widget(Label(text="Last name: "))
+        self.inside.add_widget(Label(text="Last name: "))
         self.last = TextInput(multiline=False)
-        self.add_widget(self.last)
+        self.inside.add_widget(self.last)
 
-        self.add_widget(Label(text="Email: "))
+        self.inside.add_widget(Label(text="Email: "))
         self.email = TextInput(multiline=False)
-        self.add_widget(self.email)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+        self.submit = Button(text="Submit", font_size=40)
+        self.submit.bind(on_press=self.pressed)
+        self.add_widget(self.submit)
+
+    def pressed(self, instance):
+        name = self.name.text
+        last = self.last.text
+        email = self.email.text
+
+        self.name.text = ""
+        self.last.text = ""
+        self.email.text = ""
 
 
 class MyApp(App):
