b21914b590cac8eb4af1349c3e447c0aba8888ce
Witchoutname <folyn.tomas1@gmail.com>
2021-02-14 22:03:54 +0100

    float

--- main.py
+++ main.py
@@ -2,30 +2,16 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
 
-class MyGrid(Widget):
-    name = ObjectProperty(None)
-    last = ObjectProperty(None)
-    email = ObjectProperty(None)
-
-    def pressed(self):
-        name = self.name.text
-        last = self.last.text
-        email = self.email.text
-
-        self.name.text = ""
-        self.last.text = ""
-        self.email.text = ""
-
-
 class MyApp(App):
     def build(self):
-        return MyGrid()
+        return FloatLayout()
 
 
 MyApp().run()
