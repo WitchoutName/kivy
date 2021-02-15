147070fb89a8262d5cf7e4034eca1f102a7850f5
Witchoutname <folyn.tomas1@gmail.com>
2021-02-15 10:50:59 +0100

    draw

--- main.py
+++ main.py
@@ -7,16 +7,22 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.graphics import *
 
 
 class Touch(Widget):
-    btn = ObjectProperty(None)
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
+
+        with self.canvas:
+            Color(1,0,0,1,mode="rgba")
 
     def on_touch_down(self, touch):
+        self.rect.pos = touch.pos
         self.btn.opacity = 0.5
 
     def on_touch_move(self, touch):
-        pass
+        self.rect.pos = touch.pos
 
     def on_touch_up(self, touch):
         self.btn.opacity = 1
