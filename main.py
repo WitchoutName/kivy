3bed69881375f6488e09a191ed4347dcbf14585c
Witchoutname <folyn.tomas1@gmail.com>
2021-02-15 10:44:39 +0100

    opacity

--- main.py
+++ main.py
@@ -9,9 +9,22 @@ from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
 
 
+class Touch(Widget):
+    btn = ObjectProperty(None)
+
+    def on_touch_down(self, touch):
+        self.btn.opacity = 0.5
+
+    def on_touch_move(self, touch):
+        pass
+
+    def on_touch_up(self, touch):
+        self.btn.opacity = 1
+
+
 class MyApp(App):
     def build(self):
-        return FloatLayout()
+        return Touch()
 
 
 MyApp().run()
